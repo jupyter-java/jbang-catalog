e8b133272e04433d284e36a3b95f6186ee244e0a
Max Rydahl Andersen <max@xam.dk>
2025-09-12 11:04:37 +0200

    fix: enable test exec

--- .github/test.sh
+++ .github/test.sh
@@ -7,7 +7,7 @@ while read -r file; do
         echo "Error building $file"
         has_error=1
     fi
-done < <(jq -r '.aliases[]["script-ref"] | select(contains(".java"))' jbang-catalog.json)
+done < <(jq -r '.aliases[]["script-ref"] | select(contains(".java"))' ../jbang-catalog.json)
 
 echo "has_error: $has_error"
 exit $has_error
